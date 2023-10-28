40e370d554fb42770844c5138030dc99461534f8
gaura <gauravchoudhary.1191@gmail.com>
2023-10-27 23:35:37 -0400

    This is first commit

---
+++ src/main/java/com/example/springeaplab5/SpringEapLab5Application.java
@@ -0,0 +1,13 @@
+package com.example.springeaplab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEapLab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEapLab5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springeaplab5/SpringEapLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springeaplab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEapLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
